c34cd676496543509a9985b77003cb748487a2ba
Unknown <hfelton@gmail.com>
2018-08-01 03:06:35 +0800

    eventdates tables working
    basic table generation for event dates info...

--- modules/EventDates.lua
+++ modules/EventDates.lua
@@ -25,7 +25,7 @@ local EventNames = { -- abbr to actual-names
                     is = 'Isolated Station' ,
                     eeq = 'Event-Equilibrium' ,
                     ms = 'Missing' ,
-                    -- catchall-bad
+                    -- catchall-bad (numeric-key vs. alphabetic)
                     'Unnamed Event' ,
                   }
 
@@ -40,15 +40,16 @@ local EventDates = { -- abbr to known-dates
                     		'2017-12-13', 	'2018-02-21', 	'2018-05-02',
                     	} ,
                     vi = {
-                    		'2018-05-16',
+                    		'2018-05-16',	'2018-07-11',
                     	} ,
                     is = {
                     		'2017-02-01', 	'2017-05-17', 	'2017-08-30',
-                    		'2017-11-22', 	'2018-03-07', 	'2018-04-18'
+                    		'2017-11-22', 	'2018-03-07', 	'2018-04-18',
+                    		'2018-07-25',
                     	} ,
                     eeq = {
                     		'2017-02-22', 	'2017-06-07', 	'2017-09-20',
-                    		'2018-01-03',   '2018-05-30',   
+                    		'2018-01-03',   '2018-05-30',
                     	} ,
                     ms = {
                     		'2017-03-15', 	'2017-06-28', 	'2017-10-11',
@@ -57,60 +58,69 @@ local EventDates = { -- abbr to known-dates
                     'generic date = ccYY-MM-dd' ,
                   }
 
+
+
 local function mkTable(inpT,hdrT,flgT)
 	local retOut,inputTable,headerTable
 	local rowHdr,rowDat
+	local miniTbl = {}
+	local chkHdrs
+	local hdrSzs = { 10, 30, 50, }
 	local possFlags = { "flgClass", "flgAutoNumRows", "flgHeaderDetails", }
 	local dbgOut = '\n\n'
-	dbgOut = dbgOut .. 'begin-dbgOut... \n\n'
-	dbgOut = dbgOut .. 'flag-Table is \n\n'
-	dbgOut = dbgOut .. utils.dumpTable(flgT) ..'\n\n'
-	dbgOut = dbgOut .. 'header-Table is \n\n'
-	dbgOut = dbgOut .. utils.dumpTable(hdrT) ..'\n\n'
-	dbgOut = dbgOut .. 'input-Table is \n\n'
-	dbgOut = dbgOut .. utils.dumpTable(inpT) ..'\n\n'
-	dbgOut = dbgOut .. 'end-dbgOut... \n\n '
+
 	retOut = mw.html.create('table')
+	retOut:attr( 'border', '1')
+
 	-- check and DO the flag-Table
 	for _,flg in ipairs(possFlags) do
 		if flg == 'flgClass' then
-			-- 'wikitable sortable'
+			-- override class for options like 'wikitable sortable'
 			if utils.isNotBlank(flgT[flg]) then
 				retOut:addClass(flgT[flg])
 			else
-				retOut:addClass('wikitable')
+				retOut:addClass('articletable')
+			end
+		end
+		if flg == 'flgHeaderDetails' then
+			-- only deal with one-item...
+			if utils.isNotBlank(flgT[flg]) then
+				chkHdrs=true
+				table.insert(hdrSzs,1,10)
+			else
+				chkHdrs=false
 			end
 		end
 		if flg == 'flgAutoNumRows' then
-			-- adjust inputTable, headerTable for first-column-numbering...
+			-- adjust headerTable for first-column-numbering...
 			if utils.isNotBlank(flgT[flg]) then
 				local nr = 0
-				-- local miniTbl
-				-- headerTable = {}
 				table.insert(hdrT,1,"###")
-				-- headerTable = table.insert(hdrT,1,"###")
-				headerTable = utils.tableShallowCopy(hdrT)
-				inputTable = {}
-				for k,v in pairs(inpT) do
+			-- THEN adjust inputTable for first-column-numbering...
+				-- require inpT to have UNnamed rows of table-data...
+				for k,v in ipairs(inpT) do
 					nr = nr + 1
-					-- miniTbl = {nr}
-					-- working-assumption that v can act-as-a-table...
-					inputTable[k] = table.insert(v,1,nr)
+					table.insert(v,1,nr)
+					miniTbl[k] = utils.tableShallowCopy(v)
 				end
-			else
-				headerTable = utils.tableShallowCopy(hdrT)
-				inputTable = utils.tableShallowCopy(inpT)
+				-- overwrite original...
+				inpT = miniTbl
 			end
 		end
 	end
+	headerTable = utils.tableShallowCopy(hdrT)
+	inputTable = utils.tableShallowCopy(inpT)
+
 	-- once flags are done, do the headers...
-	if not glbls.dbg then
+	if glbls.dbg then
 	dbgOut = '\n\n'
 	dbgOut = dbgOut .. 'begin-dbgOut... \n\n'
 	dbgOut = dbgOut .. 'flag-Table is \n\n'
 	dbgOut = dbgOut .. utils.dumpTable(flgT) ..'\n\n'
 	dbgOut = dbgOut .. 'header-Table is \n\n'
 	dbgOut = dbgOut .. utils.dumpTable(hdrT) ..'\n\n'
+	dbgOut = dbgOut .. 'hdrSzs-Table is \n\n'
+	dbgOut = dbgOut .. utils.dumpTable(hdrSzs) ..'\n\n'
 	dbgOut = dbgOut .. 'input-Table is \n\n'
 	dbgOut = dbgOut .. utils.dumpTable(inpT) ..'\n\n'
 	dbgOut = dbgOut .. 'end-dbgOut... \n\n '
@@ -123,29 +133,43 @@ local function mkTable(inpT,hdrT,flgT)
 		FmtHdr = utils.TitleCase(headerTable[hdr])
 		-- FmtHdr = hdr:upper()
 		-- FmtHdr = hdr:lower()
-		rowHdr
-			:tag('th')
-			:wikitext(FmtHdr)
-			-- :css('width','20%')
-			-- :attr('scope','col')
-			-- :attr('data-sort-type','numeric')
-			:done()
+		-- if i try to break-out the rowHdr props, it fails...
+		if chkHdrs then
+			rowHdr
+				:tag('th')
+				:wikitext(FmtHdr)
+				:attr('scope','col')
+				:css('width',tostring(hdrSzs[hdr])..'%')
+				:done()
+		else
+			rowHdr
+				:tag('th')
+				:wikitext(FmtHdr)
+				:attr('scope','col')
+				-- :attr('data-sort-type','numeric')
+				-- :css('width','50%')
+				:done()
+		end
 	end
 	retOut:node(rowHdr)
 	-- once headers are done, do the actual rows...
-	for k,v in pairs(inputTable) do
+	for k,v in ipairs(inputTable) do
+		local subTbl, subStr
 		if type(v) == 'table' then
 			local rowDat = mw.html.create('tr')
 			for dat = 1, #v do
+				if type(v[dat]) == 'table' then
+					subTbl = v[dat]
+					subStr = utils.dumpTable(subTbl)
+				else
+					subStr = tostring(v[dat])
+				end
 				rowDat
 					:tag('td')
-					:wikitext(tostring(dat))
-					-- :wikitext(tostring(v[dat]))
+					:wikitext(subStr)
 					:done()
 			end
 			retOut:node(rowDat)
-		--else
-			retOut:node(rowHdr)
 		end
 	end
 	retOut:node(rowHdr)
@@ -188,21 +212,38 @@ function p.main(frame)
     local tempstr=''
     if what=='index' then
         out = mkIndex()
-    elseif (what=='Events') or (what=='EventsDebug') then
+    elseif (what=='Events')
+						or (what=='EventsDebug')
+						or (what=='EventsAddRowNums') then
 		if what=='EventsDebug' then
 			glbls.dbg = true
 		end
 		local iT,hT,fT
-		fT = { flgClass='wikitable, sortable', flgAutoNumRows='true', }
+		local junkStr
+		if what=='EventsAddRowNums' then
+			fT = { flgClass='wikitable, sortable', flgAutoNumRows='true', flgHeaderDetails='true',}
+		else
+		--	fT = {}
+			fT = { flgClass='wikitable', flgHeaderDetails='true',}
+		end
 		hT = { 'abbr', 'name', 'datesList', }
-		iT = { 	{ 'ev9', 'event Nine', {'dt9', 'dt8', 'dt7',}, },
-				{ 'ev4', 'event Four', {'dt2', 'dt3', 'dt4',}, },
-				{ 'ev5', 'event Five', {'dt5', 'dt6', 'dt7',}, },
-				{ 'ev1', 'event One', {'dt0', 'dt1', 'dt2',}, },
-			}
+		iT = {}
+		for seq = 1,#EventSeq do
+			local abr = EventSeq[seq]
+			iT[seq] = { abr, EventNames[abr], EventDates[abr], }
+		end
+--		iT = { 	{ 'ev9', 'event Nine', {'dt9', 'dt8', 'dt7',}, },
+--				{ 'ev4', 'event Four', {'dt2', 'dt3', 'dt4',}, },
+--				{ 'ev5', 'event Five', {'dt5', 'dt6', 'dt7',}, },
+--				{ 'ev1', 'event One', {'dt0', 'dt1', 'dt2',}, },
+--			}
 		out = mw.html.create()
 		hold = mkTable(iT,hT,fT)
+--		junkStr = addParagraph('junk Paragraph...')
+--		out:node(junkStr)
+--		out:node(junkStr)
 		out:node(hold)
+--		out:node(junkStr)
     else
         out = "Hello, world! - doing..."..tostring(what)..
             "... with ==>"..utils.dumpTable(glbls.data)
